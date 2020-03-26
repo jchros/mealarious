51875710c17ddec7040cc145921e043d74c292a6
kloKenPhantasie <nuocsa@icloud.com>
2020-03-26 10:42:06 -0400

    Création d'un fichier de test
    Ce commit a pour but de:
    - vérifier que la coordination GitHub et mi-phpmut fonctionne
    - s'assurer que l'on puisse prendre les coordonées SQL en dehors du git
    - s'assurer que le repo ne soit pas vide :)

---
+++ test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>
+Mealarious — Test
+</title>
+<meta charset="utf-8"/>
+</head>
+
+<body>
+<?php
+require '../credentials.php';
+echo "<ul>";
+echo "\t<li>" . LOGIN  . "</li>\n";
+echo "\t<li>" . DB     . "</li>\n";
+echo "\t<li>" . SERVER . "</li>\n";
+echo "</ul>\n";
+?>
+</body>
+</html>
