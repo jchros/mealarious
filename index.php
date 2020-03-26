a24f8166dce5a01507a784bbda76a3db419bf87c
kloKenPhantasie <nuocsa@icloud.com>
2020-03-26 11:18:03 -0400

    Création de index.php
    Ce commit est également l'occasion de tester si la configuration git de
    mon ordinateur fonctionne.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+<meta charset="UTF-8"/>
+<title>
+Mealarious
+</title>
+</head>
+
+<body>
+<?php
+echo "<p>\n
+Tout vient à point à qui sait attendre!\n
+</p>\n";
+echo "<p>\n
+Nous sommes en plein développement de ce site Web.\n
+Revenez plus tard quand ce site sera plus complet.\n
+</p>\n";
+?>
+</body>
+</html>
